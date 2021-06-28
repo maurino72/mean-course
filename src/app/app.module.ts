a1546f780fbd1f415861d4c5d18d2fc3ebe04c53
Bruno Maurino <brunomaurino27@gmail.com>
2021-06-28 07:45:20 -0300

    Outputting Posts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PostListComponent } from './components/posts/post-list/post-list.componnet';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PostCreateComponent } from './components/posts/post-create/post-create.component';
@@ -7,12 +8,18 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, PostCreateComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    HeaderComponent,
+    PostListComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
@@ -21,6 +28,7 @@ import { FormsModule } from '@angular/forms';
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/posts/post-list/post-list.componnet.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent {
+  postList = [
+    {
+      title: 'First Post title',
+      description: 'This is the first post content',
+    },
+    {
+      title: 'Second Post title',
+      description: 'This is the second post content',
+    },
+    {
+      title: 'Third Post title',
+      description: 'This is the third post content',
+    },
+  ];
+}
