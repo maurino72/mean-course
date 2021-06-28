0a515c45d0961d911d876a4ad46c3f4bdb92282a
Bruno Maurino <brunomaurino27@gmail.com>
2021-06-28 07:29:13 -0300

    Adding Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PostCreateComponent } from './components/posts/post-create/post-create.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, PostCreateComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
