ba2e0c7b30b73c58a3fa9ab34758536590f01029
Bruno Maurino <brunomaurino27@gmail.com>
2021-06-28 08:45:44 -0300

    Creating Post Service

--- src/app/components/posts/post-list/post-list.componnet.ts
+++ src/app/components/posts/post-list/post-list.componnet.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Post } from '../post.model';
+import { PostsService } from './../posts.service';
 
 @Component({
   selector: 'app-post-list',
@@ -22,4 +23,6 @@ export class PostListComponent {
   //   },
   // ];
   @Input() posts: Post[] = [];
+
+  constructor(public postService: PostsService) {}
 }
